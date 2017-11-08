cbd1eac518ad83b496b58706122f11993e94268c
johan Gustafsson <johanrg70@gmail.com>
2017-11-08 13:57:24 +0100

    Added access-control-allow-origin header to API module

--- backend/module/API/src/Module.php
+++ backend/module/API/src/Module.php
@@ -3,11 +3,31 @@
 namespace API;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Mvc\MvcEvent;
 
 class Module implements ConfigProviderInterface
 {
   public function getConfig()
   {
     return include __DIR__ . '/../config/module.config.php';
+  }
+
+  public function onBootstrap(MvcEvent $event)
+  {
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+      header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+      header('Access-Control-Allow-Credentials: true');
+      header('Access-Control-Max-Age: 86400');
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+      }
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+      }
+      exit(0);
+    }
   }
 }
