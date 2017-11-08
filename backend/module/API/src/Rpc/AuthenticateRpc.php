ee709dfad7243d397539ca779be779859dfbd759
johan Gustafsson <johanrg70@gmail.com>
2017-11-08 16:12:41 +0100

    Added skeleton authentication rpc to backend

--- backend/module/API/src/Rpc/AuthenticateRpc.php
+++ backend/module/API/src/Rpc/AuthenticateRpc.php
@@ -8,6 +8,14 @@ class AuthenticateRpc extends RestfulAuthenticationController
 {
   public function authenticateAction()
   {
-    return $this->restResponse->createResponse(200, '', ['result' => true]);
+    $username = $this->params()->fromQuery('username');
+    $password = $this->params()->fromQuery('password');
+    $payload = [
+      'isAdmin' => true,
+      'exp' => time() + 43200 // 12 Hours before expire
+    ];
+
+    $token = $this->authentication->encode($payload);
+    return $this->restResponse->createResponse(200, '', ['token' => $token]);
   }
 }
